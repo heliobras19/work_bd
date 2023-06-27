a8e07ff045f3f6056c7d6f1e38707c197bebe106
josedomingos919 <josedomingos919@gmail.com>
2023-06-28 00:00:28 +0100

    feat: save list all tables

--- src/folha_salario/bd.php
+++ src/folha_salario/bd.php
@@ -2,6 +2,8 @@
 
 $mysqli = new mysqli("localhost", "root", "Domingos.1", "folha_salario");
 
+mysqli_set_charset($mysqli, "utf8");
+
 // Check connection
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
